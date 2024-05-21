b6fc4baa06b5596fcf9f244f17159c8c076c1181
MACEIROS <Maceirosfernando@hotmail.com>
2024-05-20 22:46:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Limbert Maceiros</title>
+    <style>
+        .pie{
+            background: darkblue;
+            border-radius: 15px;
+            color: white;
+            font-size: 13px;
+        }
+        .barra{
+            background: rgb(0, 0, 0);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <img src="imagenes/banner.jpg" class="responsive">
+        </div>
+        <div class="row">
+            <div class="col-md-2 d-grid gap-2">
+                <a href="#" class="btn btn-secondary">MENÚ</a>
+                <a href="#" class="btn btn-danger">Primer Bimestre</a>
+                <a href="noticias/" class="btn btn-info" target="_blank">Noticias</a>
+                <a href="CRUD/" class="btn btn-info" target="_blank">CRUD PHP</a>
+                <a href="#" class="btn btn-danger">Segundo Bimestre</a>
+                <a href="jquery/" class="btn btn-info">JQUERY</a>
+                <a href="calculadora/" class="btn btn-info">Calculadora</a>
+                <a href="facebook/" class="btn btn-info">Facebook</a>
+                <a href="crudS/" class="btn btn-info">NUEVO CRUD</a>
+            </div>
+            <br><br><br>
+            <div class="col-md-8">
+                <div class="">
+                <center><h3>Presentacion: </h3><p>
+                    Bienvenidos en este apartado estare publicando a lo largo del año las practicas de Diseño web del Licenciado Alejandro Guzman
+                </p></center>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3>Otros</h3>
+            </div>
+        </div>
+        <br>
+        <div class="row pie">
+            <br>
+            <center>Desarrollado por un Técnico Medio del Área de Sistemas<br>
+            &copy; LFMV(2024)</center>
+        </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
